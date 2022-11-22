ef255a3e5d9ae06ccff843f9b3b206e720a28e86
Gareth Kwan <gareth0201@gmail.com>
2022-11-21 23:08:15 -0500

    Worked on server.js

--- server.js
+++ server.js
@@ -1,64 +1,66 @@
 //Import and require modules needed for the project
-const express = require("express");
 const mysql = require("mysql2");
 const connection = require("./config/connection");
 const inquirer = require("inquirer");
 
 const PORT = process.env.PORT || 3000;
-const app = express();
-
-// // Middleware
 
 //Inquirer function to begin the list of choices using SQL
-async function runInquirer() {
-  let prompter = await inquirer.prompt([
-    {
-      name: "choice",
-      type: "list",
-      message: "What would you like to do?",
-      choices: [
-        "View all department",
-        "View all role",
-        "View all employees",
-        "View all employees by department",
-        "View all employees by manager",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update employee role",
-        "Update employee manager",
-        "Delete a department",
-        "Delete a role",
-        "Delete an employee",
-      ],
-    },
-  ]);
-
-  console.log(prompter);
-  let { choice } = prompter;
-
-  console.log(choice);
-  switch (choice) {
-    case "View all department":
-      viewAllDepartment();
-      break;
-    case "View all role":
-      viewAllEmployees();
-      break;
-    default:
-      runInquirer();
-      break;
-  }
+function runInquirer() {
+  inquirer
+    .prompt([
+      {
+        name: "choice",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all department",
+          "View all role",
+          "View all employees",
+          "View all employees by department",
+          "View all employees by manager",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update employee role",
+          "Update employee manager",
+          "Delete a department",
+          "Delete a role",
+          "Delete an employee",
+        ],
+      },
+    ])
+    .then(({ choice }) => {
+      console.log(choice);
+      switch (choice) {
+        case "View all department":
+          viewAllDepartment();
+          break;
+        case "View all role":
+          viewAllRoles();
+          break;
+        case "View all employees":
+          viewAllRoles();
+          break;
+        default:
+          runInquirer();
+          break;
+      }
+    });
 }
 
 // View all departments
 function viewAllDepartment() {
   console.log("view all deparments was selected");
-  let results = connection.query("SELECT * FROM department;", function (error, department) {
-    if (error) throw error;
-    console.table(department);
-    runInquirer();
-  });
+  let results = connection.query(
+    "SELECT * FROM department;",
+
+    function (error, department) {
+      if (error) throw error;
+      console.table(department);
+      runInquirer();
+    }
+  );
 }
 
 // View all roles
